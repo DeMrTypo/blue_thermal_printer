7a112edd291b4dce9b189f97dc35a4cd05b6c4ad
donny <donnyjulianto186@gmail.com>
2026-01-20 10:49:21 +0700

    set min sdk to 28

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "id.kakzaki.blue_thermal_printer_example"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 29
+        minSdk = 28
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
